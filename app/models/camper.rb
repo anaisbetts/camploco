cda4f042c31d68e714c93e6495fe334049d536d2
Paul Betts <paul@paulbetts.org>
2008-04-06 23:18:32 -0700

    Add merit badge info to the camper model

--- app/models/camper.rb
+++ app/models/camper.rb
@@ -1,2 +1,68 @@
+MeritBadges = {
+  "Monday & Tuesday (9:00 to 10:45)" => {
+    "Archery" => [],
+    "Astronomy" => [],
+    "Aviation" => [],
+    "Basketry" => [],
+    "BSA Lifeguard" => [2,3,4],
+    "Canoeing" => [],
+    "COPE" => [2],
+    "Emergency Prep" => [],
+    "Leatherwork" => [],
+    "Lifesaving" => [],
+    "Nicoteh" => [2,3],
+    "Pioneering" => [2],
+    "Rifle" => [],
+    "Small Boat Sailing" => [],
+    "Swimming" => [],
+    "System Management" => [3]
+  }, 
+
+  "Monday & Tuesday (11:00 to 12:45)" => {
+    "Archery" => [],
+    "Camping" => [],
+    "Creature Studies" => [4],
+    "Environmental Science" => [],
+    "First Aid" => [4],
+    "Leatherwork" => [],
+    "Lifesaving" => [],
+    "Photojournalism" => [],
+    "Rowing" => [],
+    "Shotgun" => [],
+    "Swimming" => [],
+    "Woodcarving" => []
+  }, 
+
+  "Thursday & Friday (9:00 to 10:45)" => {
+    "Archery" => [],
+    "Basketry" => [],
+    "Camping" => [],
+    "Canoeing" => [],
+    "Climbing" => [],
+    "Cycling" => [],
+    "Emergency Prep" => [],
+    "Environmental Science" => [],
+    "Indian Lore" => [],
+    "Lifesaving" => [],
+    "Orienteering" => [4],
+    "Rifle" => [],
+    "Rowing" => [],
+    "Swimming" => []
+  }, 
+
+  "Thursday & Friday (11:00 to 12:45)" => {
+    "Archery" => [],
+    "Canoeing" => [],
+    "Hiking and Backpacking" => [],
+    "Lifesaving" => [],
+    "Pottery" => [],
+    "Shotgun" => [],
+    "Small Boat Sailing" => [],
+    "Space Exploration" => [],
+    "Swimming" => [],
+    "Woodcarving" => [],
+  }
+}
+
 class Camper < ActiveRecord::Base
 end
