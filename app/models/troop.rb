698e15a8fa11deb92c6c07200a5ebdb14df9ae54
Paul Betts <paul@paulbetts.org>
2008-04-06 22:23:43 -0700

    Add columns to troops, add header to pages

--- app/models/troop.rb
+++ app/models/troop.rb
@@ -1,2 +1,9 @@
+AvailableWeeks = {
+  Date.parse("2008-06-24") => "Week 1: June 24th - June 30th",
+  Date.parse("2008-07-01") => "Week 1: July 1st - July 7th",
+  Date.parse("2008-07-08") => "Week 1: July 8th - July 14th",
+  Date.parse("2008-07-15") => "Week 1: July 15th - July 21th"
+}
+
 class Troop < ActiveRecord::Base
 end
--- db/migrate/001_create_troops.rb
+++ db/migrate/001_create_troops.rb
@@ -1,10 +1,17 @@
 class CreateTroops < ActiveRecord::Migration
   def self.up
     create_table :troops do |t|
-      t.integer :number
-      t.string :troopmaster
-      t.string :email
       t.string :identity_url
+
+      t.string :troopmaster
+      t.string :troopmaster_address
+      t.string :troopmaster_city
+      t.integer :troopmaster_zipcode
+      t.string :troopmaster_email
+
+      t.integer :number
+      t.string :district
+      t.string :council
 
       t.timestamps
     end
