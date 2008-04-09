f06a17e5b1b15c34fd7fcf5093a84711b42160bd
Paul Betts <paul@paulbetts.org>
2008-04-08 20:34:57 -0700

    Fix select on new troop view - validation always fails for session for some reason though...

--- app/models/troop.rb
+++ app/models/troop.rb
@@ -1,14 +1,23 @@
-AvailableWeeks = {
-  Date.parse("2008-06-24") => "Week 1: June 24th - June 30th",
-  Date.parse("2008-07-01") => "Week 1: July 1st - July 7th",
-  Date.parse("2008-07-08") => "Week 1: July 8th - July 14th",
-  Date.parse("2008-07-15") => "Week 1: July 15th - July 21th"
+AvailableSessionsAsDate = {
+  1 => Date.parse("2008-06-24"), 
+  2 => Date.parse("2008-07-01"),
+  3 => Date.parse("2008-07-08"), 
+  4 => Date.parse("2008-07-15") 
+}
+
+AvailableSessionsAsText = {
+  1 => "Week 1: June 24th - June 30th",
+  2 => "Week 2: July 1st - July 7th",
+  3 => "Week 3: July 8th - July 14th",
+  4 => "Week 4: July 15th - July 21th"
 }
 
 EmailRegex = /[A-Z0-9._%-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/
 ZipcodeRegex = /^\d{5}([\-]\d{4})?$/
 
 class Troop < ActiveRecord::Base
+  has_many :campers
+
   validates_presence_of :troopmaster
   validates_presence_of :troopmaster_address
   validates_presence_of :troopmaster_city
@@ -17,4 +26,15 @@ class Troop < ActiveRecord::Base
   validates_numericality_of :number, :greater_than => 0
   validates_presence_of :district
   validates_presence_of :council
+  #validates_presence_of :session
+
+  def session_text=(v)
+    ret = AvailableSessionsAsText.keys.find {|x| (AvailableSessionsAsText[x] == v) and x }
+    session = ret
+  end
+
+  def session_text
+    AvailableSessionsAsText[session]
+  end
+
 end
