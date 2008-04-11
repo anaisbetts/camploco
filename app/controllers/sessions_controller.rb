0d43b6bfe89732c8dabab58e1d8a8a491449f375
Paul Betts <paul@paulbetts.org>
2008-04-10 22:54:26 -0700

    Banging my head against OpenID redirection stuff

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,17 +1,33 @@
 class SessionsController < ApplicationController
+  include AuthenticatedSystem
   def create
     open_id_authentication
-  end                                                                                                                                                         
+  end
+
+  def show
+    # TODO: Figure out how to redirect to where we were previously
+    #redirect_to :controller => "troops", :action => "show"
+    #debugger
+  end
 
   def open_id_authentication
     authenticate_with_open_id do |result, identity_url|
+      debugger
       if result.successful?
-        if @current_user = @account.users.find_by_identity_url(identity_url)
+        if @current_user = User.find_by_identity_url(identity_url)
           successful_login
         else
-          failed_login "Sorry, no user by that identity URL exists (#{identity_url})"
+          # Auto-create the new user
+          new_user = User.new
+          new_user.identity_url = identity_url
+          new_user.save
+          @current_user = User.find_by_identity_url(identity_url)
+          #redirect_to("http://www.google.com")
+
+          successful_login
         end
       else
+        #redirect_to("http://www.yahoo.com")
         failed_login result.message
       end
     end
@@ -21,11 +37,16 @@ class SessionsController < ApplicationController
   private
   def successful_login
     session[:user_id] = @current_user.id
+    #redirect_to("http://www.msn.com")
+    #debugger
     redirect_to(root_url)
   end
 
   def failed_login(message)
     flash[:error] = message
+    #redirect_to("http://www.slashdot.org")
+    foo = "test"
+    #debugger
     redirect_to(new_session_url)
   end
 end
--- app/controllers/troops_controller.rb
+++ app/controllers/troops_controller.rb
@@ -1,3 +1,5 @@
 class TroopsController < ApplicationController
+  before_filter :login_required
+
   resources_controller_for :troops
 end
