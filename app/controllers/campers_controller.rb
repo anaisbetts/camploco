b2b1e587d9769b5916d7d98725435305a4b760fc
Paul Betts <paul@paulbetts.org>
2008-04-12 15:34:36 -0700

    Nuke MooTable, use TableKit instead

--- app/controllers/campers_controller.rb
+++ app/controllers/campers_controller.rb
@@ -1,3 +1,9 @@
 class CampersController < ApplicationController
+  before_filter :set_troop
+
   resources_controller_for :campers
+
+  def set_troop
+    @troop = Troop.find_by_id(params[:troop_id])
+  end
 end
