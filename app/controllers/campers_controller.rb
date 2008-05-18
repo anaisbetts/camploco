b2542248c4e7c97edf74bf6c312fe138f0a09fe5
Paul Betts <paul@paulbetts.org>
2008-05-17 22:26:24 -0700

    Add AJAX to session drop-downs

--- app/controllers/campers_controller.rb
+++ app/controllers/campers_controller.rb
@@ -1,4 +1,23 @@
 class CampersController < ApplicationController
   before_filter :login_required
   resources_controller_for :campers, :in => :troop
+  skip_before_filter :load_enclosing_resources, :only => :disable_sessions
+
+  def disable_sessions
+    # Set the MB stuff so we can get the enabled sessions
+    fake = Camper.new
+
+    c = params["camper"]
+    fake.meritbadge0_text = c["meritbadge0_text"]
+    fake.meritbadge1_text = c["meritbadge1_text"]
+    fake.meritbadge2_text = c["meritbadge2_text"]
+    fake.meritbadge3_text = c["meritbadge3_text"]
+    fake.meritbadge4_text = c["meritbadge4_text"]
+
+    @enabled_list = (0..4).collect {|x| fake.slot_enabled? x}
+
+    respond_to do |wants|
+      wants.js
+    end
+  end
 end
