ccd86d9c75c1c85225c01a9ceb1389885fc3124e
Paul Betts <paul@paulbetts.org>
2009-05-10 20:59:07 -0700

    Remove all auth stuff

--- app/helpers/campers_helper.rb
+++
@@ -1,2 +0,0 @@
-module CampersHelper
-end
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 21) do
+ActiveRecord::Schema.define(:version => 22) do
 
   create_table "campers", :force => true do |t|
     t.string   "name"
@@ -33,34 +33,6 @@ ActiveRecord::Schema.define(:version => 21) do
     t.datetime "updated_at"
   end
 
-  create_table "open_id_authentication_associations", :force => true do |t|
-    t.integer "issued"
-    t.integer "lifetime"
-    t.string  "handle"
-    t.string  "assoc_type"
-    t.binary  "server_url"
-    t.binary  "secret"
-  end
-
-  create_table "open_id_authentication_nonces", :force => true do |t|
-    t.integer "timestamp",  :null => false
-    t.string  "server_url"
-    t.string  "salt",       :null => false
-  end
-
-  create_table "permissions", :force => true do |t|
-    t.integer  "role_id",    :null => false
-    t.integer  "user_id",    :null => false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "roles", :force => true do |t|
-    t.string   "rolename"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "troops", :force => true do |t|
     t.string   "troopmaster"
     t.string   "troopmaster_address"
@@ -76,22 +48,6 @@ ActiveRecord::Schema.define(:version => 21) do
     t.string   "troopmaster_state"
     t.integer  "user_id"
     t.boolean  "hidden"
-  end
-
-  create_table "users", :force => true do |t|
-    t.string   "login"
-    t.string   "email"
-    t.string   "crypted_password",          :limit => 40
-    t.string   "salt",                      :limit => 40
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "remember_token"
-    t.datetime "remember_token_expires_at"
-    t.string   "activation_code",           :limit => 40
-    t.datetime "activated_at"
-    t.string   "password_reset_code",       :limit => 40
-    t.boolean  "enabled",                                 :default => true
-    t.string   "identity_url"
   end
 
 end
